9996295a77e0be87fb50efe88f600cbd410ae9dd
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-10-31 12:48:26 -0300

    chore: change query

--- src/app/api/youtubeWorld.tsx
+++ src/app/api/youtubeWorld.tsx
@@ -1,7 +1,7 @@
 export default async function YoutubeWorld() {
   const apiKey = process.env.YOUTUBE_API_KEY;
   const maxResults = 20;
-  const searchQuery = "a day in the life of a software engineer";
+  const searchQuery = "day in the life of a software engineer";
 
   const fetchVideos = await fetch(
     `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${searchQuery}&order=date&type=video&videoDuration=medium&key=${apiKey}`
