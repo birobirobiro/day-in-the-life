37e82fe897692c1356c8dee12ed337624cb24806
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-10-31 11:39:05 -0300

    fix: types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,14 @@ import YoutubeWorld from "./api/youtubeWorld";
 import YoutubeBR from "./api/youtubeBR";
 import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
+import {
+  Key,
+  ReactElement,
+  JSXElementConstructor,
+  ReactNode,
+  ReactPortal,
+  PromiseLikeOfReactNode,
+} from "react";
 
 export default async function Home() {
   const worldVideos = await YoutubeWorld();
@@ -28,93 +36,168 @@ export default async function Home() {
 
         <TabsContent value="videosWorld" className="mt-10">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {worldVideos.items.map((video) => (
-              <Card key={video.id}>
-                <CardHeader>
-                  <CardTitle className="leading-relaxed">
-                    {video.snippet.title}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <iframe
-                    className="w-full rounded-lg"
-                    height="315"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                  ></iframe>
+            {worldVideos.items.map(
+              (video: {
+                id: { videoId: string } | null | undefined;
 
-                  <div className="flex gap-2 pt-4">
-                    <Badge variant="secondary">
-                      {video.snippet.channelTitle}
-                    </Badge>
-                    <Badge variant="secondary">
-                      {new Date(video.snippet.publishTime).toLocaleDateString(
-                        "pt-BR",
-                        {
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        }
-                      )}
-                    </Badge>
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <CardDescription className="flex flex-col gap-2 mb-2">
-                    <span className="leading-relaxed">
-                      {video.snippet.description}
-                    </span>
-                  </CardDescription>
-                </CardFooter>
-              </Card>
-            ))}
+                snippet: {
+                  title:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | ReactPortal
+                    | PromiseLikeOfReactNode
+                    | null
+                    | undefined;
+                  channelTitle:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | ReactPortal
+                    | PromiseLikeOfReactNode
+                    | null
+                    | undefined;
+                  publishTime: string | number | Date;
+                  description:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | ReactPortal
+                    | PromiseLikeOfReactNode
+                    | null
+                    | undefined;
+                };
+              }) => (
+                <Card key={video.id?.videoId}>
+                  <CardHeader>
+                    <CardTitle className="leading-relaxed">
+                      {video.snippet.title}
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <iframe
+                      className="w-full rounded-lg"
+                      height="315"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      src={`https://www.youtube.com/embed/${video.id?.videoId}`}
+                    ></iframe>
+
+                    <div className="flex gap-2 pt-4">
+                      <Badge variant="secondary">
+                        {video.snippet.channelTitle}
+                      </Badge>
+                      <Badge variant="secondary">
+                        {new Date(video.snippet.publishTime).toLocaleDateString(
+                          "pt-BR",
+                          {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                          }
+                        )}
+                      </Badge>
+                    </div>
+                  </CardContent>
+                  <CardFooter>
+                    <CardDescription className="flex flex-col gap-2 mb-2">
+                      <span className="leading-relaxed">
+                        {video.snippet.description}
+                      </span>
+                    </CardDescription>
+                  </CardFooter>
+                </Card>
+              )
+            )}
           </div>
         </TabsContent>
 
         <TabsContent value="videosBR" className="mt-10">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            {brVideos.items.map((video) => (
-              <Card key={video.id}>
-                <CardHeader>
-                  <CardTitle className="leading-relaxed">
-                    {video.snippet.title}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <iframe
-                    className="w-full rounded-lg"
-                    height="315"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    src={`https://www.youtube.com/embed/${video.id.videoId}`}
-                  ></iframe>
+            {brVideos.items.map(
+              (video: {
+                id: { videoId: string } | null | undefined;
+                snippet: {
+                  title:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | ReactPortal
+                    | PromiseLikeOfReactNode
+                    | null
+                    | undefined;
+                  channelTitle:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | ReactPortal
+                    | PromiseLikeOfReactNode
+                    | null
+                    | undefined;
+                  publishTime: string | number | Date;
+                  description:
+                    | string
+                    | number
+                    | boolean
+                    | ReactElement<any, string | JSXElementConstructor<any>>
+                    | Iterable<ReactNode>
+                    | ReactPortal
+                    | PromiseLikeOfReactNode
+                    | null
+                    | undefined;
+                };
+              }) => (
+                <Card key={video.id?.videoId}>
+                  <CardHeader>
+                    <CardTitle className="leading-relaxed">
+                      {video.snippet.title}
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <iframe
+                      className="w-full rounded-lg"
+                      height="315"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                      src={`https://www.youtube.com/embed/${video.id?.videoId}`}
+                    ></iframe>
 
-                  <div className="flex gap-2 pt-4">
-                    <Badge variant="secondary">
-                      {video.snippet.channelTitle}
-                    </Badge>
-                    <Badge variant="secondary">
-                      {new Date(video.snippet.publishTime).toLocaleDateString(
-                        "pt-BR",
-                        {
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        }
-                      )}
-                    </Badge>
-                  </div>
-                </CardContent>
-                <CardFooter>
-                  <CardDescription className="flex flex-col gap-2 mb-2">
-                    <span className="leading-relaxed">
-                      {video.snippet.description}
-                    </span>
-                  </CardDescription>
-                </CardFooter>
-              </Card>
-            ))}
+                    <div className="flex gap-2 pt-4">
+                      <Badge variant="secondary">
+                        {video.snippet.channelTitle}
+                      </Badge>
+                      <Badge variant="secondary">
+                        {new Date(video.snippet.publishTime).toLocaleDateString(
+                          "pt-BR",
+                          {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                          }
+                        )}
+                      </Badge>
+                    </div>
+                  </CardContent>
+                  <CardFooter>
+                    <CardDescription className="flex flex-col gap-2 mb-2">
+                      <span className="leading-relaxed">
+                        {video.snippet.description}
+                      </span>
+                    </CardDescription>
+                  </CardFooter>
+                </Card>
+              )
+            )}
           </div>
         </TabsContent>
       </Tabs>
