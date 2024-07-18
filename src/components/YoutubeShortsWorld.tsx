230a32f7935f978908c7b692b9bb508b9f9ded77
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-07-18 14:57:25 -0300

    fix: TS errors

--- src/components/YoutubeShortsWorld.tsx
+++ src/components/YoutubeShortsWorld.tsx
@@ -20,14 +20,55 @@ import {
 } from "react";
 import YoutubeShortsWorld from "@/app/api/youtubeShortsWorld";
 
+type Video = {
+  id: { videoId: string } | null | undefined;
+  snippet: {
+    title:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | Iterable<ReactNode>
+      | ReactPortal
+      | PromiseLikeOfReactNode
+      | null
+      | undefined;
+    channelTitle:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | Iterable<ReactNode>
+      | ReactPortal
+      | PromiseLikeOfReactNode
+      | null
+      | undefined;
+    publishTime: string | number | Date;
+    description:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | Iterable<ReactNode>
+      | ReactPortal
+      | PromiseLikeOfReactNode
+      | null
+      | undefined;
+  };
+};
+
+type VideoResponse = {
+  items: Video[];
+};
+
 export default function VideosShortsWorld() {
-  const [shortVideosWorld, setShortVideosWorld] = useState(null);
+  const [shortVideosWorld, setShortVideosWorld] = useState<VideoResponse | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchShortVideos = async () => {
       try {
-        const data = await YoutubeShortsWorld();
+        const data: VideoResponse = await YoutubeShortsWorld();
         setShortVideosWorld(data);
       } catch (error) {
         console.error("Failed to fetch short videos", error);
@@ -49,84 +90,47 @@ export default function VideosShortsWorld() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {shortVideosWorld.items.map(
-        (video: {
-          id: { videoId: string } | null | undefined;
-          snippet: {
-            title:
-              | string
-              | number
-              | boolean
-              | ReactElement<any, string | JSXElementConstructor<any>>
-              | Iterable<ReactNode>
-              | ReactPortal
-              | PromiseLikeOfReactNode
-              | null
-              | undefined;
-            channelTitle:
-              | string
-              | number
-              | boolean
-              | ReactElement<any, string | JSXElementConstructor<any>>
-              | Iterable<ReactNode>
-              | ReactPortal
-              | PromiseLikeOfReactNode
-              | null
-              | undefined;
-            publishTime: string | number | Date;
-            description:
-              | string
-              | number
-              | boolean
-              | ReactElement<any, string | JSXElementConstructor<any>>
-              | Iterable<ReactNode>
-              | ReactPortal
-              | PromiseLikeOfReactNode
-              | null
-              | undefined;
-          };
-        }) => (
-          <Card key={video.id?.videoId}>
-            <CardHeader>
-              <CardTitle className="leading-relaxed truncate">
-                {video.snippet.title}
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <iframe
-                className="w-full rounded-lg"
-                height="500"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                src={`https://www.youtube.com/embed/${video.id?.videoId}`}
-              ></iframe>
+      {shortVideosWorld.items.map((video) => (
+        <Card key={video.id?.videoId}>
+          <CardHeader>
+            <CardTitle className="leading-relaxed truncate">
+              {video.snippet.title}
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <iframe
+              className="w-full rounded-lg"
+              height="500"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              src={`https://www.youtube.com/embed/${video.id?.videoId}`}
+            ></iframe>
 
-              <div className="flex gap-2 pt-4">
-                <Badge variant="secondary" className="text-center">
-                  {video.snippet.channelTitle}
-                </Badge>
-                <Badge variant="secondary" className="text-center">
-                  {new Date(video.snippet.publishTime).toLocaleDateString(
-                    "pt-BR",
-                    {
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    }
-                  )}
-                </Badge>
-              </div>
-            </CardContent>
-            <CardFooter>
-              <CardDescription className="flex flex-col gap-2 mb-2">
-                <span className="leading-relaxed">
-                  {video.snippet.description}
-                </span>
-              </CardDescription>
-            </CardFooter>
-          </Card>
-        )
-      )}
+            <div className="flex gap-2 pt-4">
+              <Badge variant="secondary" className="text-center">
+                {video.snippet.channelTitle}
+              </Badge>
+              <Badge variant="secondary" className="text-center">
+                {new Date(video.snippet.publishTime).toLocaleDateString(
+                  "pt-BR",
+                  {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  }
+                )}
+              </Badge>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <CardDescription className="flex flex-col gap-2 mb-2">
+              <span className="leading-relaxed">
+                {video.snippet.description}
+              </span>
+            </CardDescription>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   );
 }
