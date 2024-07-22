9a84fa864a33e9c3471ee6546ff17d8da91bc690
João Inácio Neto <birobirobiro.dev@gmail.com>
2024-07-22 17:20:54 -0300

    update sort search

--- src/app/api/youtubeShortsBR.tsx
+++ src/app/api/youtubeShortsBR.tsx
@@ -1,10 +1,10 @@
 export default async function YoutubeShortsBR(pageToken = '') {
   const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
   const maxResults = 20;
-  const searchQuery = "rotina programador";
+  const searchQuery = "um dia na vida de um programador|rotina de programador";
 
   const fetchVideos = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&maxResults=${maxResults}&q=${searchQuery}&order=date&type=video&videoDuration=short&pageToken=${pageToken}&key=${apiKey}`
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&videoEmbeddable=true&maxResults=${maxResults}&q=${encodeURIComponent(searchQuery)}&order=date&type=video&videoDuration=short&pageToken=${pageToken}&key=${apiKey}&relevanceLanguage=pt`
   );
 
   if (!fetchVideos.ok) {
